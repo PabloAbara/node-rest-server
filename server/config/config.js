34a4825cf0fd0e1c63cbb21569a74d826a1ff093
pabs <pbabara@uc.cl>
2020-07-21 17:11:09 -0400

    Rest server - Usuario

--- server/config/config.js
+++ server/config/config.js
@@ -3,3 +3,17 @@
 // puerto
 //
 process.env.PORT = process.env.PORT || 3000;
+//
+// ENTORNO
+//
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev'
+//
+// BBDDD
+//
+let urlDB;
+if(process.env.NODE_ENV === 'dev'){
+  urlDB = "mongodb://localhost:27017/cafe";
+} else {
+  urlDB = "mongodb+srv://pabs-db:gd5Zhr6bICeffhMv@cluster0.cdov3.mongodb.net/test";
+}
+process.env.URLDB = urlDB;
--- server/server.js
+++ server/server.js
@@ -1,7 +1,8 @@
 require('./config/config.js');
-// Debe ser descargado con --save
+// require('./routes/usuario.js');
+// Deben ser descargado con --save
 const express = require('express');
-// Debe ser descargado con --save
+const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const app = express();
 
@@ -11,41 +12,21 @@ app.use(bodyParser.urlencoded({ extended: false }))
 // parse application/json
 app.use(bodyParser.json())
 
+app.use(require('./routes/usuario'));
+
 // definición de servicios API
 app.get('/', function (req, res) {
   res.json('Welcome Home');
 })
 
-app.get('/usuario', function (req, res) {
-  res.json('getUsuario');
-})
-
-app.post('/usuario', function (req, res) {
-  let body = req.body;
-  if(body.nombre === undefined){
-    res.status(400).json({
-      ok:false,
-      mensaje:`La request no contiene un nombre válido`
-    })
-  } else {
-    res.json({
-      persona:body
-    });
-  }
-})
-
-app.put('/usuario/:id', function (req, res) {
-  let id = req.params.id;
-  // res.json(`putUsuario de id ${id}`);
-  res.json({
-    id
-  })
-})
-
-app.delete('/usuario', function (req, res) {
-  res.json('deleteUsuario');
-})
-
+mongoose.connect(process.env.URLDB,{
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex:true},
+  (err,res)=>{
+    if(err) throw err;
+    console.log(`BBDD online`);
+  });
 
 app.listen(process.env.PORT,()=>{
   console.log(`Escuchando puerto ${process.env.PORT}`);
